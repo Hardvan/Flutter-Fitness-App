7655632c334cc4a6e47a842e9f0dceafe99b598a
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-01-05 17:56:13 +0530

    Add left svg button

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -17,6 +18,17 @@ class HomePage extends StatelessWidget {
           ),
         ),
         centerTitle: true,
+        leading: Container(
+          width: 20,
+          height: 20,
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Color(0xffF7F8F8),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: SvgPicture.asset('assets/icons/Arrow - Left 2.svg'),
+        ),
       ),
     );
   }
